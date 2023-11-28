ae70605d2fddd592b4b4cdc9afee361df77e3af1
akinsm <akinsm@oregonstate.edu>
2023-11-27 17:57:02 -0800

    fixed db connection for customers page

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 from flask import Flask, render_template, json, redirect
 from flask_mysqldb import MySQL
 from flask import request
-from 
+from dotenv import load_dotenv, find_dotenv
 import os
-from dotenv import load_dotenv
+import database.db_connector as db
 
-load_dotenv()
+db_connection = db.connect_to_database()
+
+# load_dotenv(find_dotenv())
+
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
-app.config['MYSQL_USER'] = os.environ.get("MYSQL_USER")
-app.config['MYSQL_PASSWORD'] = os.environ.get("MYSQL_PASSWORD") 
-app.config['MYSQL_DB'] = os.environ.get("MYSQL_USER")
-app.config['MYSQL_CURSORCLASS'] = "DictCursor"
+# app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
+# app.config['MYSQL_USER'] = os.environ.get("MYSQL_USER")
+# app.config['MYSQL_PASSWORD'] = os.environ.get("MYSQL_PASSWORD") 
+# app.config['MYSQL_DB'] = os.environ.get("MYSQL_USER")
+# app.config['MYSQL_USER'] = 'cs340_akinsm'
+# app.config['MYSQL_PASSWORD'] = '1516'
+# app.config['MYSQL_DB'] = 'cs340_akinsm'
+# app.config['MYSQL_CURSORCLASS'] = "DictCursor"
 
 
 mysql = MySQL(app)
@@ -27,24 +33,30 @@ def root():
 
 @app.route('/customers', methods=["POST", "GET"])
 def customers():
-    if request.method == "GET":
-      query = "SELECT * FROM Customers;"
-      cur = mysql.connection.cursor()
-      cur.execute(query)
-      results = cur.fetchall()
-      print(results)
-      return render_template("customers.j2", data=results)
+    query = "SELECT * FROM Customers;"
+    # cur = msql.connection.cursor()
+    # cur.execute(query)
+    cur = db.execute_query(db_connection=db_connection, query=query)
+    customer_data = cur.fetchall()
+    return render_template("customers2.j2", data=customer_data)
+    # if request.method == "GET":
+    #   query = "SELECT * FROM Customers;"
+    #   cur = mysql.connection.cursor()
+    #   cur.execute(query)
+    #   results = cur.fetchall()
+    #   print(results)
+    #   return render_template("customers.j2", data=results)
     
-    if request.method == "POST":
-      if request.form.get("Add_Customer"):
-        name = request.form["name"]
-        email = request.form["email"]
-        password = request.form["password"]
-        query = "INSERT INTO Customers (name, email, password) VALUES (%s, %s,%s);"
-        cur = mysql.connection.cursor()
-        cur.execute(query, (name, email, password))
-        mysql.connection.commit()
-        return redirect("/customers")
+    # if request.method == "POST":
+    #   if request.form.get("Add_Customer"):
+    #     name = request.form["name"]
+    #     email = request.form["email"]
+    #     password = request.form["password"]
+    #     query = "INSERT INTO Customers (name, email, password) VALUES (%s, %s,%s);"
+    #     cur = mysql.connection.cursor()
+    #     cur.execute(query, (name, email, password))
+    #     mysql.connection.commit()
+    #     return redirect("/customers")
 
         
 
